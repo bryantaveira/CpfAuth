f7b8d5c35daeaabdafd3b1977ac48215a4d1a7a3
Jefferson-Bryan <bryantaveira@gmail.com>
2026-06-21 12:51:34 -0300

    add local repository

---
+++ src/main/java/com/bryandev/CpfAuth/CpfAuthApplication.java
@@ -0,0 +1,13 @@
+package com.bryandev.CpfAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryandev/CpfAuth/CpfAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryandev.CpfAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
